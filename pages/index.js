cd0dea2aff7c165e25cf61a9bb452af29885c69f
izumihara <infinity0206.tab@gmail.com>
2021-02-28 20:01:22 +0900

    componentsについて学習

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,8 @@
-import Link from 'next/link'
+import Header from '../components/Header'
 
-const Index = () => (
-    <div>
-        <Link href="/about">
-            <button>Go to About Page</button>
-        </Link>
-        <Link href='/about'>
-            <a style={{ fontSize: 20 }}>about page</a>
-        </Link>
-      <p>Hello Next.js</p>
-    </div>
-  )
-  
-  export default Index
+export default () => (
+  <div>
+    <Header />
+    <p>Hello Next.js</p>
+  </div>
+)
